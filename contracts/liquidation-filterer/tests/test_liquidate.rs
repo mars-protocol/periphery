09bf19594ccfadff9296a5e7ab4bfd3d4a8abb8c
Piotr Babel <piotr.babel@gmail.com>
2022-10-05 17:31:05 +0200

    Mp 1496 optimization (#7)
    * Fix outdated schema.
    
    * Some optimization.
    
    * Fix tests.

--- contracts/liquidation-filterer/tests/test_liquidate.rs
+++ contracts/liquidation-filterer/tests/test_liquidate.rs
@@ -1,44 +1,17 @@
 use cosmwasm_std::testing::{mock_env, mock_info};
-use cosmwasm_std::{coin, to_binary, BankMsg, CosmosMsg, Decimal, SubMsg, Uint128, WasmMsg};
+use cosmwasm_std::{coin, to_binary, BankMsg, CosmosMsg, SubMsg, Uint128, WasmMsg};
 
 use crate::helpers::{setup_test, setup_test_with_balance};
 use mars_liquidation_filterer::contract::execute;
 use mars_liquidation_filterer::error::ContractError;
 use mars_liquidation_filterer::msg::ExecuteMsg;
 use mars_liquidation_filterer::types::Liquidate;
-use mars_outpost::red_bank::{UserHealthStatus, UserPositionResponse};
 
 mod helpers;
-
-// We are only interested in health status, the rest can have random values
-fn dummy_user_position(health_status: UserHealthStatus) -> UserPositionResponse {
-    UserPositionResponse {
-        total_collateral_value: Default::default(),
-        total_debt_value: Default::default(),
-        total_collateralized_debt: Default::default(),
-        weighted_max_ltv_collateral: Default::default(),
-        weighted_liquidation_threshold_collateral: Default::default(),
-        health_status,
-    }
-}
 
 #[test]
 fn test_liquidate_many_accounts_if_missing_debt_coin() {
     let mut deps = setup_test();
-    deps.querier.set_redbank_user_position(
-        "user_address_1".to_string(),
-        dummy_user_position(UserHealthStatus::Borrowing {
-            max_ltv_hf: Decimal::percent(80),
-            liq_threshold_hf: Decimal::percent(90),
-        }),
-    );
-    deps.querier.set_redbank_user_position(
-        "user_address_2".to_string(),
-        dummy_user_position(UserHealthStatus::Borrowing {
-            max_ltv_hf: Decimal::percent(80),
-            liq_threshold_hf: Decimal::percent(90),
-        }),
-    );
 
     let info = mock_info("owner", &[coin(1234u128, "uosmo")]);
     let msg = ExecuteMsg::LiquidateMany {
@@ -69,20 +42,6 @@ fn test_liquidate_many_accounts_if_missing_debt_coin() {
 #[test]
 fn test_liquidate_many_accounts_if_not_enough_debt_coin() {
     let mut deps = setup_test();
-    deps.querier.set_redbank_user_position(
-        "user_address_1".to_string(),
-        dummy_user_position(UserHealthStatus::Borrowing {
-            max_ltv_hf: Decimal::percent(80),
-            liq_threshold_hf: Decimal::percent(90),
-        }),
-    );
-    deps.querier.set_redbank_user_position(
-        "user_address_2".to_string(),
-        dummy_user_position(UserHealthStatus::Borrowing {
-            max_ltv_hf: Decimal::percent(80),
-            liq_threshold_hf: Decimal::percent(90),
-        }),
-    );
 
     let info = mock_info("owner", &[coin(20u128, "umars"), coin(10u128, "uosmo")]);
     let msg = ExecuteMsg::LiquidateMany {
@@ -119,40 +78,10 @@ fn test_liquidate_many_accounts_if_not_enough_debt_coin() {
 #[test]
 fn test_liquidate_many_accounts() {
     let mut deps = setup_test();
-    deps.querier.set_redbank_user_position(
-        "user_address_1".to_string(),
-        dummy_user_position(UserHealthStatus::Borrowing {
-            max_ltv_hf: Decimal::percent(80),
-            liq_threshold_hf: Decimal::percent(90),
-        }),
-    );
-    deps.querier.set_redbank_user_position(
-        "user_address_2".to_string(),
-        dummy_user_position(UserHealthStatus::Borrowing {
-            max_ltv_hf: Decimal::percent(110),
-            liq_threshold_hf: Decimal::percent(120),
-        }),
-    );
-    deps.querier.set_redbank_user_position(
-        "user_address_3".to_string(),
-        dummy_user_position(UserHealthStatus::Borrowing {
-            max_ltv_hf: Decimal::percent(80),
-            liq_threshold_hf: Decimal::percent(90),
-        }),
-    );
-    deps.querier.set_redbank_user_position(
-        "user_address_4".to_string(),
-        dummy_user_position(UserHealthStatus::NotBorrowing),
-    );
 
     let info = mock_info(
         "bot",
-        &[
-            coin(1234u128, "uosmo"),
-            coin(2345u128, "umars"),
-            coin(3456u128, "ujuno"),
-            coin(7891u128, "uaxelar"),
-        ],
+        &[coin(1234u128, "uosmo"), coin(2345u128, "umars"), coin(7891u128, "uaxelar")],
     );
     let msg = ExecuteMsg::LiquidateMany {
         liquidations: vec![
@@ -174,43 +103,60 @@ fn test_liquidate_many_accounts() {
                 user_address: "user_address_3".to_string(),
                 amount: Uint128::from(891u32),
             },
-            Liquidate {
-                collateral_denom: "uatom".to_string(),
-                debt_denom: "ujuno".to_string(),
-                user_address: "user_address_4".to_string(),
-                amount: Uint128::from(456u32),
-            },
         ],
     };
-    let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
-    // user_address_2 is healthy, user_address_4 is not borrowing, additional refund message
-    assert_eq!(res.messages.len(), 3);
+    let res = execute(deps.as_mut(), mock_env(), info.clone(), msg).unwrap();
+    assert_eq!(res.messages.len(), 4);
     assert_eq!(
         res.messages[0],
-        SubMsg::new(CosmosMsg::Wasm(WasmMsg::Execute {
-            contract_addr: "red_bank".to_string(),
-            msg: to_binary(&mars_outpost::red_bank::ExecuteMsg::Liquidate {
-                collateral_denom: "uatom".to_string(),
-                user: "user_address_1".to_string()
-            })
-            .unwrap(),
-            funds: vec![coin(345u128, "umars")]
-        }))
+        SubMsg::reply_always(
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: "red_bank".to_string(),
+                msg: to_binary(&mars_outpost::red_bank::ExecuteMsg::Liquidate {
+                    collateral_denom: "uatom".to_string(),
+                    user: "user_address_1".to_string(),
+                    recipient: Some(info.sender.to_string())
+                })
+                .unwrap(),
+                funds: vec![coin(345u128, "umars")]
+            }),
+            0
+        )
     );
     assert_eq!(
         res.messages[1],
-        SubMsg::new(CosmosMsg::Wasm(WasmMsg::Execute {
-            contract_addr: "red_bank".to_string(),
-            msg: to_binary(&mars_outpost::red_bank::ExecuteMsg::Liquidate {
-                collateral_denom: "uatom".to_string(),
-                user: "user_address_3".to_string()
-            })
-            .unwrap(),
-            funds: vec![coin(891u128, "uaxelar")]
-        }))
+        SubMsg::reply_always(
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: "red_bank".to_string(),
+                msg: to_binary(&mars_outpost::red_bank::ExecuteMsg::Liquidate {
+                    collateral_denom: "uatom".to_string(),
+                    user: "user_address_2".to_string(),
+                    recipient: Some(info.sender.to_string())
+                })
+                .unwrap(),
+                funds: vec![coin(234u128, "uosmo")]
+            }),
+            1
+        )
     );
     assert_eq!(
         res.messages[2],
+        SubMsg::reply_always(
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: "red_bank".to_string(),
+                msg: to_binary(&mars_outpost::red_bank::ExecuteMsg::Liquidate {
+                    collateral_denom: "uatom".to_string(),
+                    user: "user_address_3".to_string(),
+                    recipient: Some(info.sender.to_string())
+                })
+                .unwrap(),
+                funds: vec![coin(891u128, "uaxelar")]
+            }),
+            2
+        )
+    );
+    assert_eq!(
+        res.messages[3],
         SubMsg::new(CosmosMsg::Wasm(WasmMsg::Execute {
             contract_addr: "cosmos2contract".to_string(),
             msg: to_binary(&ExecuteMsg::Refund {
