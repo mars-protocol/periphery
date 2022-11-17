1797d9d457478c4e46ef00bbb42094fab61a41b2
piobab <piotr.babel@gmail.com>
2022-11-17 09:16:08 +0100

    Remove liquidation-filterer contract. (#16)
    * Remove liquidation-filterer contract.
    
    * Remove unused files.

--- scripts/deploy/osmosis/index.ts
+++
@@ -1,6 +0,0 @@
-import { taskRunner } from '../base'
-import { osmosisAddresses, osmosisTestnetConfig } from './config.js'
-
-void (async function () {
-  await taskRunner(osmosisTestnetConfig, osmosisAddresses)
-})()
