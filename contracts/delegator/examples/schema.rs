21f34d4ef8883cdd8d7298c09956159674398cd9
larry <26318510+larry0x@users.noreply.github.com>
2023-02-18 23:10:41 +0000

    update schema again

--- contracts/delegator/examples/schema.rs
+++ contracts/delegator/examples/schema.rs
@@ -1,10 +1,11 @@
 use cosmwasm_schema::write_api;
 use cosmwasm_std::Empty;
-use mars_delegator::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use mars_delegator::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, SudoMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
+        sudo: SudoMsg,
         execute: ExecuteMsg,
         query: QueryMsg,
         migrate: Empty,
